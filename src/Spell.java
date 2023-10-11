65ff6703c2a9d6ec556457ec25be27c51c7c8f0e
DESKTOP-GHV8SML\eeist <eeistcool@gmail.com>
2023-10-11 23:42:57 +0200

    Creation spell class

---
+++ src/Spell.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+
+public class Spell extends JPanel {
+
+    GameHandler gameHandler;
+    int cost;
+    String name;
+    String descriptionEffect;
+    SpellType type;
+    PieceType mageElement;
+    JLabel infoLabel;
+    JButton performSpell;
+
+    public Spell() {
+        init();
+    }
+
+    private void init() {
+        // TODO create all spells
+        // TODO add them in spell menu
+        // TODO give effects
+        // TODO spell range
+        // TODO (show) spell path
+        // TODO spell cast limit (and increase + showing it)
+        // TODO guards blocking spells
+        // TODO show playable spells in spell menu
+        // TODO protection logic and showing in window
+        // TODO who can cast spells
+        // TODO show spell effects in the terrain
+    }
+
+    public void effect() {
+
+    }
+
+}
